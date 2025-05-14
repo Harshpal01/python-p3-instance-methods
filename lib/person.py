c8604d37f64aa1119fbca569a83eca868a8bac40
Dominic Kipkorir <harsh979pal@gmail.com>
2025-05-14 11:43:45 +0300

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
-
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+         print("The person is walking.")  
+if __name__ == "__main__":
+    fido = Person()
+    fido.talk()   
+    fido.walk()  
